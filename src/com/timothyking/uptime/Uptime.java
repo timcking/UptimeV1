38a279f822e0e4b22796319a71c932ee70255609
Tim King <timcking@gmail.com>
2013-02-17 22:46:15 -0800

    First commit

---
+++ src/com/timothyking/uptime/Uptime.java
@@ -0,0 +1,27 @@
+package com.timothyking.uptime;
+
+import java.text.DecimalFormat;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.os.SystemClock;
+
+public class Uptime extends Activity {
+    /** Called when the activity is first created. */
+	private transient TextView tv;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        tv = (TextView) findViewById(R.id.TextView01);
+        
+        float secs = SystemClock.elapsedRealtime()/1000;
+        float hours = (secs/60)/60;
+        float days = hours/24;
+        //float hours = ((SystemClock.elapsedRealtime()/1000)/60)/60;
+        
+        DecimalFormat df = new DecimalFormat("#####.##");
+        tv.setText(df.format(hours) + " hours\nOr\n" + df.format(days) + " days");
+    }
+}
